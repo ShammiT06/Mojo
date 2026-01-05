8a871a845671d3e7bccda266db1fea73b473bf24
ShammiT06 <shammikumar.fullstack@gmail.com>
2026-01-05 18:40:46 +0530

    updated

--- src/Components/Reusable Components/Header.jsx
+++ src/Components/Reusable Components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../../assets/Logo/logo.png";
+import logo from "../../assets/Logo/Logo.png"
 import { Link } from "react-router-dom";
 
 export default function Header() {
